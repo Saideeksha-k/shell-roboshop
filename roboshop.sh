9d81128bcf768725f566a8042633c3cfb20c3984
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-09 18:05:58 +0530

    actions

---
+++ roboshop.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+SG_ID="sg-0d19e8684e6f602f7"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do 
+    INSTANCE_ID=$( aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' \
+    --query 'Instances[0].InstanceId' \
+    --output text )
+
+    if [ $instance == "frontend"]; then
+         IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PublicIpAddress' \
+             --output text
+
+         )
+
+    else
+          IP=$(
+            aws ec2 describe-instances \
+            --instance-ids $INSTANCE_ID \
+            --query 'Reservations[].Instances[].PrivateIpAddress' \
+             --output text
+          )
+    fi
+
+    echo ip adress is $IP
+          
+    
+done
