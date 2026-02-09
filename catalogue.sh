0853d2b411424381537027149c52438cd37b743d
Kayasiri Sai Deeksha <deeksha@Kayasiris-MacBook-Air.local>
2026-02-09 20:03:11 +0530

    actions

--- catalogue.sh
+++ catalogue.sh
@@ -33,12 +33,14 @@ VALIDATE $? "Enabling nodejs 20"
 
 dnf install nodejs -y &>>$LOGS_FILE
 VALIDATE $? "Install nodejs"
+
 id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
      useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
      VALIDATE $? "creating system user"
 else
     echo -e "Roboshop user already exist...$Y skipping $N"
+fi
 
 mkdir -p /app 
 VALIDATE $? "creating directory"
